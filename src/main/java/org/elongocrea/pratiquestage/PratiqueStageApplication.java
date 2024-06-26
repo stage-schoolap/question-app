b0118431e28579a371b0a5735ecf78f0ca8c8697
merdielongo <merdielongo9@gmail.com>
2024-06-26 13:37:50 +0100

    setup project

---
+++ src/main/java/org/elongocrea/pratiquestage/PratiqueStageApplication.java
@@ -0,0 +1,13 @@
+package org.elongocrea.pratiquestage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PratiqueStageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PratiqueStageApplication.class, args);
+    }
+
+}
